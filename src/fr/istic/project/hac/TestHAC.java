b0d7e5a2ae7816804c0882de3adad8ee6257e5fe
toane <mr-maurice@wanadoo.fr>
2013-02-12 10:55:29 +0100

    embryon de truc de test d'algos

---
+++ src/fr/istic/project/hac/TestHAC.java
@@ -0,0 +1,124 @@
+package fr.istic.project.hac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestHAC {
+
+	/**
+	 * executer ce script pour generer des couples nom / timestamp depuis un dossier de photo
+	 * puis coller le résultat en dessous
+	 * for i in *.JPG;do echo -n dat.add\(new Valuefist\(\"$i\""," ;date -r $i '+%s));' ;done
+	 */
+	public static void main(String[] args) {
+
+		new TestHAC();
+	}
+
+	private List<Valuefist> dat;
+
+	public TestHAC(){
+		dat=new ArrayList<Valuefist>();
+		dat.add(new Valuefist("CIMG0760.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0761.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0762.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0763.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0766.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0767.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0768.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0769.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0770.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0771.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0772.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0774.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0775.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0776.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0777.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0778.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0780.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0781.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0783.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0784.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0786.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0787.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0788.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0789.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0790.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0791.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0792.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0793.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0794.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0795.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0796.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0797.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0798.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0799.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0800.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0801.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0802.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0803.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0804.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0805.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0806.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0807.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0808.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0809.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0810.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0811.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0812.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0813.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0814.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0815.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0816.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0818.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0819.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0820.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0821.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0822.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0823.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0824.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0825.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0826.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0827.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0828.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0829.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0830.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0831.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0832.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0835.JPG",1309470974));
+		dat.add(new Valuefist("CIMG0836.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0837.JPG",1309470970));
+		dat.add(new Valuefist("CIMG0838.JPG",1309470972));
+		dat.add(new Valuefist("CIMG0840.JPG",1309470976));
+		dat.add(new Valuefist("CIMG0760.JPG",1309470970));
+
+		fucktepate();
+
+	}
+
+	private void fucktepate(){
+		for(Valuefist one: dat){
+			for(Valuefist two: dat){
+				System.out.println(one.getName()+" "+two.getName()+" "+String.valueOf((one.getTime()-two.getTime())));
+			}
+		}
+
+	}
+	private class Valuefist{
+		String name;
+		int time;
+		private List<Valuefist> distances;
+		public Valuefist(String name,int time){
+
+
+			this.name=name;
+			this.time=time;
+		}
+		public String toString(){
+			return "name "+name+" timestamp "+time;
+		}
+		public String getName(){return name;}
+		public int getTime(){return time;}
+
+	}
+}
