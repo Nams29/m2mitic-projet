1bcf699a376ac72de84279f102dd6102cbee8743
Nolwenn <nolwenn.stephan@gmail.com>
2013-02-12 18:19:30 +0100

    Premier essai de branche de timeline = trucs chelous

--- src/fr/istic/project/controller/TimeLineActivity.java
+++ src/fr/istic/project/controller/TimeLineActivity.java
@@ -4,18 +4,26 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import fr.istic.project.R;
 import fr.istic.project.utils.UIUtils;
 
 public class TimeLineActivity extends Activity {
-
+	
+	private LinearLayout rlItem1;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timeline);
-
+		
+		this.initLayout();
+		this.loadData();
+		
 		if (UIUtils.isHoneycomb()) {
 			ActionBar actionBar = this.getActionBar();
 			actionBar.setDisplayHomeAsUpEnabled(true);
@@ -35,6 +43,23 @@ public class TimeLineActivity extends Activity {
 		else {
 			return super.onOptionsItemSelected(item);
 		}
+	}
+	
+	/**
+	 * Initializes the graphic elements
+	 */
+	private void initLayout() {
+		this.rlItem1 = (LinearLayout) this.findViewById(R.id.timeline_item1);
+	}
+	
+	/**
+	 * Load data on the fields
+	 */
+	private void loadData() {
+		LayoutInflater factory = LayoutInflater.from(this);
+		View child = factory.inflate(R.layout.item_timeline, null);
+		
+		this.rlItem1.addView(child);
 	}
 
 }
