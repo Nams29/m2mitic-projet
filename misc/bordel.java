2af59456cf5a47dda8eaaae3d999f41d8fa43768
florian <florian@florian-netbook>
2013-02-14 01:21:26 +0100

    Archivage de code au cas ou

--- misc/bordel.java
+++ misc/bordel.java
@@ -2,7 +2,23 @@
   * BDD : 				http://stackoverflow.com/questions/9109438/how-to-use-an-existing-database-with-an-android-application/9109728#9109728
   * Image similarity : 	http://stackoverflow.com/questions/843972/image-comparison-fast-algorithm
   */
-	 
+
+
+// UTILISATION D'UNE LIGNE HORIZONTALE ET D'UNE LIGNE VERTICALE EN PLEIN MILIEU (FORME UN +)  
+int height = bitmap.getHeight();
+int width = bitmap.getWidth();
+byte[] bitmapByteArray = new byte[height+width];
+System.out.println(bitmapByteArray.length);
+for (int x=0; x<width; x++) { // Ligne -
+	int pixel = bitmap.getPixel(x, height/2);
+	bitmapByteArray[x] = (byte) pixel;
+}
+for (int y=0; y<height; y++) { // Ligne |
+	int pixel = bitmap.getPixel(width/2, y);
+	bitmapByteArray[y+width] = (byte) pixel;
+}	
+
+
 	 
 /* MEDIASTORE */
 String[] projection = { MediaStore.Images.Media._ID, MediaStore.Images.ImageColumns.DATA };
