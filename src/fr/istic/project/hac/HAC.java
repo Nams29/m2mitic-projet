53b0a3000a16bbdfec2e4f0120603be03eda37c0
toane <mr-maurice@wanadoo.fr>
2013-02-14 01:41:39 +0100

    implementation d'un algo de regroupement hierarchique, pas encore utilisable, utilisant la distance de ward pour les regroupements

--- src/fr/istic/project/hac/HAC.java
+++ src/fr/istic/project/hac/HAC.java
@@ -3,106 +3,108 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * mode d'emploi: instancier la classe
- * intancier autant d'objets PictInfo que necessaire
- * et les ajouter avec addPict
+ * ajouter avec addPict autant que necessaire
  * @author antoine
  *
  */
 public class HAC {
 	private final static int NBCLASSES=4;
-	/**
-	 * executer ce script pour generer des couples nom / timestamp depuis un dossier de photo
-	 * puis coller le résultat en dessous entre //ici et //la
-	 * OBSOLETE
-	 * 
-	#/bin/bash
-	function ex {
-	exiv2 $i|grep timestamp|cut -d:  -f2-5;
-	}
-
-	for i in *.JPG;
-	do 
-	echo -n dat.add\(new CrabeMou\(\"$i\""," ;echo \"`ex`\" \)\)";";
-	done
-
-	 * le script affiche les distances entre chaque couple de photo
-	 * compatible avec un algo de hac
-	 */
-
-	private List<PictInfo> dat;
-	private CustomArrayList dist;
-	//	private List<Long> mids;//valeurs moyennes pour établir les classes
-	private List<CustomArrayList> clusters;
-	private List<CustomArrayList> clusters_copy;
-
+	private ArrayList<ArrayList<PictInfo>> dat;
 	/**
 	 * ajoute des PictInfo a analyser
 	 * @param name nom du fichier image
 	 * @param timestamp date de prise de vue au format yyyy:MM:DD HH:mm
 	 */
 	public void addPict(String name,String timestamp){
-		dat.add(new PictInfo(name,timestamp));
+		ArrayList<PictInfo> t=new ArrayList<PictInfo>();
+		PictInfo pi=new PictInfo(name,timestamp);
+		t.add(pi);
+		dat.add(t);
 	}
-	public HAC(){
-		dat=new ArrayList<PictInfo>();//utilisé une fois pour parcourir la liste des images et calculer les distances
-		dist=new CustomArrayList();//<-- TODO rendre ça générique
-		clusters=new ArrayList<HAC.CustomArrayList>();//clusters 1 -----> * CustomArrayList 1 -----> * Distances 
-	}
-	
-	public void printResults(){
 
-		for (CustomArrayList clu:clusters){
-			System.out.println("UN CLUSTER");
-			for (Distance d:clu){
-				System.out.println(d);
-			}
-		}
+	public HAC(){
+		dat=new ArrayList<ArrayList<PictInfo>>();
 	}
 
 	public void findMiddle(){
-		long distance=0;
-		CustomArrayList[] halfs;
-		for(PictInfo one: dat){
-			for(PictInfo two: dat){
-				distance=one.getTime()-two.getTime();
-				if(distance>=0){//hypothèse: si une distance est négative son équivalent positif sera calculé ailleurs
-					dist.add(new Distance(one, two,distance));//la moitié des objets du tableau sont des doublons
+		/**
+		 * Pour i=1 à individus.longueur Faire
+    	   classes.ajouter(nouvelle classe(individu[i]));
+			Fin Pour
+		 */
+		float[][] dissim=null;
+		while(dat.size()>NBCLASSES){
+			dissim =new float[dat.size()][dat.size()];
+			for (int i=0;i<dat.size();i++){
+				for (int j=i+1;j<dat.size();j++){
+					dissim[i][j]=dissim(dat.get(i),dat.get(j));
 				}
 			}
-		}
-		clusters.add(dist);//on démarre avec tout dans le même cluster
-		for(int i=0;i<NBCLASSES;i++){
-			clusters_copy=new ArrayList<HAC.CustomArrayList>();//on édite une copie de la liste
-			System.out.println("PASS"+i+" "+clusters.size());
-			for (CustomArrayList cal:clusters){//on parcourt la liste originale
-				halfs=halve(cal);//recupere deux moitiés de cluster, classés par rapport a la valeur moyenne
-				if(halfs[0].size()>0){clusters_copy.add(halfs[0]);
-				System.out.println("\t ajout de "+halfs[0].size());
+			int i=0,j=0;
+			float min=dissim[0][dat.size()-1];
+
+			for (int k=0;k<dat.size();k++){
+				for (int l=k+1;l<dat.size();l++){
+					if( (dissim[k][l]<min)&&(dissim[k][l]>0)){
+						min=dissim[k][l];i=k;j=l;//TODO pas sur que ca sorte vraiment le minimum
+						System.out.println("found min="+min+"@ k:"+k+" l:"+l+" ");
+					}
 				}
-
-				if(halfs[1].size()>0){clusters_copy.add(halfs[1]);
-				System.out.println("\t ajout de "+halfs[1].size());
-				}
-
-
 			}
-
-			clusters=clusters_copy;
+			List<PictInfo> toAdd = new ArrayList<PictInfo>();
+			for(Iterator<PictInfo> it = dat.get(j).iterator(); it.hasNext();) {
+				PictInfo element = it.next();
+				toAdd.add(element);
+			}           
+			dat.get(i).addAll(toAdd);
+			dat.remove(j);//ne suffit pas pour virer une colonne
+			graph(dissim);
 		}
-		graph(clusters);
+	}
+	public void graph(float[][] dist){
+		for(int i=0;i<dist[0].length;i++){
+			System.out.print("|");
+			for(int j=0;j<dist[i].length;j++){
+				System.out.print(dist[i][j]+" ");
+			}
+			System.out.println("|");
+		}
+	}
+	public float dissim(ArrayList<PictInfo> ap1,ArrayList<PictInfo> ap2){
+		float sum_ap1=0;float sum_ap2=0;
+		float mean_ap1=0;float mean_ap2=0;
+		float ward=0;
+		for(PictInfo p:ap1){
+			sum_ap1=sum_ap1+p.getTime();
+		}
+		mean_ap1=sum_ap1/ap1.size();//calcul de la moyenne ap1
+
+		for(PictInfo p:ap2){
+			sum_ap2=sum_ap2+p.getTime();
+		}
+		mean_ap2=sum_ap2/ap2.size();//calcul de la moyenne ap2
+		float sap1=ap1.size();
+		float sap2=ap2.size();
+
+		ward= ((sap1*sap2)/(sap1+sap2))*(mean_ap2-mean_ap1);//calcul de la distance de ward
+		return ward;
 
 	}
-	private void graph(List<CustomArrayList> l){
+
+	/**
+	 * 
+	 * @param l une ArrayList qui contient des ArrayList
+	 */
+	private void graph(List<PictInfo> l){
 		long sum=0;
-		for(CustomArrayList o:l){
-			System.out.print("["+o.size()+"]");
-			sum+=o.size();
+		for(PictInfo o:l){
+			System.out.print("["+o.getName()+"]");
 		}
-		System.out.println("--------");
 	}
 
 	/**
@@ -113,10 +115,8 @@ public class HAC {
 	class PictInfo{
 		long time;// no see
 		String name;
-
 		public PictInfo(String name,String strDate){
-
-			SimpleDateFormat formatter = new SimpleDateFormat(" yyyy:MM:DD HH:mm");
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy:MM:DD HH:mm");
 			Date dateStr=new Date();
 			try {
 				dateStr = formatter.parse(strDate);
@@ -132,67 +132,6 @@ public class HAC {
 		}
 		public String getName(){return name;}
 		public long getTime(){return time;}
-	}
-
-	class Distance{
-		private PictInfo p1;
-		private PictInfo p2;
-		private long distance;
-		public long getDistance() {
-			return distance;
-		}
-		@Override
-		public String toString() {
-			return "Distance [p1=" + p1.getName() + ", p2=" + p2.getName() + ", distance="
-					+ distance + "]";
-		}
-		public Distance(PictInfo p1,PictInfo p2,long distance){
-			this.p1=p1;this.p2=p2;this.distance=distance;
-		}
-	}
-	/**
-	 * extends ArrayList, mais peut renvoyer la moyenne des distances en un coup
-	 * 
-	 */
-	class CustomArrayList extends ArrayList<Distance>{
-		private static final long serialVersionUID = 1L;
-
-		public long getMiddle(){
-			long sum=0;long res=0;
-			for(Distance d:this){
-				sum=sum+d.getDistance();
-			}
-			try{
-				res=sum/this.size();
-			}
-			catch(ArithmeticException e){}
-			return res;
-		}
-	}
-
-
-
-	/**
-	 * 
-	 * @return les valeurs de la liste, avec les valeurs sup a middle d'un coté, inf de l'autre
-	 */
-	public CustomArrayList[] halve(CustomArrayList cal){
-		long mid=cal.getMiddle();
-
-		CustomArrayList inf = new CustomArrayList();
-		CustomArrayList sup = new CustomArrayList();
-		for(Distance d:cal){
-			if (d.getDistance()>mid){//les distances supérieures a la moyenne vont dans sup
-				sup.add(d);
-			}else{//sinon dans inf
-				inf.add(d);
-			}
-		}
-		CustomArrayList[] ret = new CustomArrayList[2];
-		ret[0]=inf;
-		ret[1]=sup;
-		System.out.println("halve(): recu "+cal.size()+"; sup "+sup.size()+" items; inf "+inf.size()+" items");
-		return ret;
 	}
 }
 
--- src/fr/istic/project/hac/HACTest.java
+++ src/fr/istic/project/hac/HACTest.java
@@ -12,15 +12,15 @@ public class HACTest {
 	}
 	public HACTest(){
 		HAC hac=new HAC();
-		hac.addPict("CIMG0760.JPG"," 2010:03:05 23:50" );
-		hac.addPict("CIMG0761.JPG"," 2010:03:05 23:50" );
-		hac.addPict("CIMG0762.JPG"," 2010:03:05 23:52" );
-		hac.addPict("CIMG0763.JPG"," 2010:03:05 23:52" );
-		hac.addPict("CIMG0766.JPG"," 2010:03:05 23:55" );
-		hac.addPict("CIMG0767.JPG"," 2010:03:05 23:55" );
-		hac.addPict("CIMG0768.JPG"," 2010:03:05 23:55" );
+		hac.addPict("CIMG0760.JPG","2010:03:05 23:50" );
+		hac.addPict("CIMG0761.JPG","2010:03:05 23:50" );
+		hac.addPict("CIMG0762.JPG","2010:03:05 23:50" );
+		hac.addPict("CIMG0763.JPG","2010:03:05 23:52" );
+		hac.addPict("CIMG0766.JPG","2010:03:05 23:52" );
+		hac.addPict("CIMG0767.JPG","2010:03:05 23:55" );
+
 		
 		hac.findMiddle();
-		hac.printResults();
+//		hac.printResults();
 	}
 }
