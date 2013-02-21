8ed4599199941599ff2e4f22e7eab4414d2c4165
florian <florian@florian-netbook>
2013-02-21 20:12:57 +0100

    cleanup TODOs

--- src/fr/istic/project/data/FindPhotosTask.java
+++ src/fr/istic/project/data/FindPhotosTask.java
@@ -41,8 +41,8 @@ public class FindPhotosTask extends AsyncTask<File, Integer, Void> {
 		new ThreadPoolExecutor(
 			maxThreads,
 			maxThreads,
-			1, // TODO optimisation ?
-			TimeUnit.MINUTES, // TODO optimisation ?
+			1,
+			TimeUnit.MINUTES,
 			new ArrayBlockingQueue<Runnable>(maxThreads, true),
 			new ThreadPoolExecutor.CallerRunsPolicy()
 	);
@@ -69,9 +69,10 @@ public class FindPhotosTask extends AsyncTask<File, Integer, Void> {
 		this.geocoderError = false;
 		
 		this.applicationDB = activity.getApplicationDB();
-		applicationDB.setAllPhotosUnavailable(); // TODO compter avec  queryNumEntries
-		if (applicationDB.getContext(0) == null) // TODO a corriger ?
+		applicationDB.setAllPhotosUnavailable();
+		if (applicationDB.getContext(0) == null) { // TODO a corriger ?
 			OContext.defaultContext = applicationDB.addContext(OContext.defaultContext); // Ajout du contexte par défaut
+		}
 	}
 	
 	@Override
@@ -165,7 +166,7 @@ public class FindPhotosTask extends AsyncTask<File, Integer, Void> {
 		long duration = System.currentTimeMillis() - start;
 		
 		
-		Toast.makeText(activity, "duree : "+ (duration/1000), Toast.LENGTH_LONG).show();
+		//Toast.makeText(activity, "duree : "+ (duration/1000), Toast.LENGTH_LONG).show();
 		activity.onPostExecuteFindPhotosTask(newPhotos); // Retourne dans l'activity        
     }
 }
